4000bd49ea25d155d91ef2283535c89d8e8d02f5
Christian Chiarulli <chris.machine@pm.me>
2025-04-12 01:28:51 -0400

    start fresh

--- src/components/posts/PostSatCount.tsx
+++
@@ -1,7 +0,0 @@
-export default function PostSatCount() {
-  return (
-    <span className="text-xxs font-light text-zinc-500 dark:text-zinc-400">
-      100 sats
-    </span>
-  );
-}
--- src/nostr-query/lib/pool.ts
+++
@@ -1,5 +0,0 @@
-import { SimplePool } from 'nostr-tools';
-
-const defaultPool = new SimplePool();
-
-export default defaultPool;
